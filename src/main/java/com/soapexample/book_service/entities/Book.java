98801dd6ea628ff5de78dd620c7b09709f676e32
Pragati <pragati.patidar@codetechinfosystem.com>
2025-06-05 12:03:21 +0530

    WIP:CURRENT CHANGES TO Book.java

--- src/main/java/com/soapexample/book_service/entities/Book.java
+++ src/main/java/com/soapexample/book_service/entities/Book.java
@@ -11,7 +11,7 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+private double price;
     private String title;
     private String author;
 
